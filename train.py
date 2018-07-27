2092960d10fa95dba4f38fadec0402ea5e8161e2
Neil You <neilyou@qq.com>
2018-07-27 16:05:36 +0800

    add GMM reparameterization trick to train generator

--- train.py
+++ train.py
@@ -12,12 +12,14 @@ import tensorflow.contrib.slim as slim
 import tensorflow.contrib.rnn as rnn
 import multiprocessing
 from tensorpack.tfutils.scope_utils import auto_reuse_variable_scope
+from wgan import WGANTrainer
 
 MAX_SEQ_LEN = 250
 BATCH_SIZE = 100
 LSTM_F_SIZE = 256
 LSTM_G_SIZE = 512
 Z_DIM = 128
+GMM_COMPONENTS = 20
 
 
 class MyDataflow(RNGDataFlow):
@@ -25,13 +27,14 @@ class MyDataflow(RNGDataFlow):
         self.data_set = dataset
 
     def get_data(self):
-        yield self.data_set.random_batch(self.rng)[:2]
+        b = self.data_set.random_batch(self.rng)
+        yield [b[2], b[1]]
 
 
 class WGAN(ModelDesc):
     def inputs(self):
         return [tf.placeholder(tf.int32, [None], 'seq_len'),
-                tf.placeholder(tf.float32, [None, MAX_SEQ_LEN, 5], 'strokes5')]
+                tf.placeholder(tf.float32, [None, MAX_SEQ_LEN + 1, 5], 'strokes5')]
 
     # B * MAX_LEN * 5
     @auto_reuse_variable_scope
@@ -41,8 +44,8 @@ class WGAN(ModelDesc):
             cell_bw = rnn.BasicLSTMCell(LSTM_F_SIZE)
             init_state_fw = cell_fw.zero_state(BATCH_SIZE, tf.float32)
             init_state_bw = cell_bw.zero_state(BATCH_SIZE, tf.float32)
-            _, output_state_fw, output_state_bw = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw,
-                                                                                 tf.unstack(strokes, axis=1), seq_len, init_state_fw,
+            _, (output_state_fw, output_state_bw) = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw,
+                                                                                 strokes, seq_len, init_state_fw,
                                                                                  init_state_bw)
             # print(output_state_fw[1].shape)
 
@@ -63,16 +66,42 @@ class WGAN(ModelDesc):
                                                 tf.zeros([BATCH_SIZE, LSTM_G_SIZE], name='hz'),
                                                 shape=[None, LSTM_G_SIZE], name='h'))
             seq_input = tf.concat([tf.tile(tf.expand_dims(z, 1), [1, MAX_SEQ_LEN, 1]), strokes], 2)
-            seq_output = tf.nn.static_rnn(cell, seq_input, init_state)
+            seq_output, _ = tf.nn.static_rnn(cell, tf.unstack(seq_input, axis=1), init_state)
+            seq_output = tf.stack(seq_output, axis=1)
             return seq_output
+
+    def sample_GMM(self, rnn_output):
+        shape = rnn_output.shape
+        rnn_output_reshaped = tf.reshape(rnn_output, [-1, shape[-1]])
+        x = slim.fully_connected(rnn_output_reshaped, GMM_COMPONENTS * 6 + 3, activation_fn=None)
+        q = tf.nn.softmax(x[:, -3:])
+        pi, ux, uy, sigmax, sigmay, rhoxy = tf.split(tf.reshape(x[:, :-3], [-1, GMM_COMPONENTS, 6]), 6, axis=2)
+        pi = tf.nn.softmax(tf.squeeze(pi, 2))
+        ux = tf.squeeze(ux, 2)
+        uy = tf.squeeze(uy, 2)
+        sigmax = tf.exp(tf.squeeze(sigmax, 2))
+        sigmay = tf.exp(tf.squeeze(sigmay, 2))
+        rhoxy = tf.nn.tanh(tf.squeeze(rhoxy, 2))
+        sigmaxy = sigmax * sigmay
+
+        # reparameterization trick
+        z = tf.random_normal([x.shape.as_list()[0], GMM_COMPONENTS, 2, 1])
+        tau = sigmax * sigmax + sigmay * sigmay
+        s = sigmaxy * tf.sqrt(1 - rhoxy * rhoxy)
+        t = tf.sqrt(tau + 2 * s)
+        sigma = tf.reshape(tf.expand_dims(1 / t, -1) * tf.stack([sigmax * sigmax + s, rhoxy * sigmaxy, rhoxy * sigmaxy, sigmay * sigmay + s], -1), [t.shape.as_list()[0], t.shape.as_list()[1], 2, 2])
+        z_sample = tf.squeeze(sigma @ z, -1) + tf.stack([ux, uy], -1)
+        xy_gmm = tf.reduce_sum(tf.expand_dims(pi, -1) * z_sample, 1)
+
+        return tf.reshape(tf.concat([xy_gmm, q], -1), [shape[0], shape[1], 5])
 
     def build_graph(self, seq_len, strokes):
         z_f = tf.random_normal([BATCH_SIZE, Z_DIM])
-        diff = self.f(seq_len, strokes) - self.f(seq_len, self.g(z_f, strokes))
+        diff = self.f(seq_len, strokes[:, 1:]) - self.f(seq_len, self.sample_GMM(self.g(z_f, strokes[:, :-1])))
         self.f_loss = -tf.reduce_mean(diff, name='f_loss')
 
         z = tf.placeholder_with_default(tf.random_normal([BATCH_SIZE, Z_DIM]), [BATCH_SIZE, Z_DIM], name='z')
-        self.g_loss = -tf.reduce_mean(self.f(seq_len, self.g(z, strokes)), name='g_loss')
+        self.g_loss = -tf.reduce_mean(self.f(seq_len, self.sample_GMM(self.g(z, strokes[:, :-1]))), name='g_loss')
 
         add_moving_summary(self.f_loss, self.g_loss)
 
@@ -82,7 +111,7 @@ class WGAN(ModelDesc):
     def optimizer(self):
         lr = tf.get_variable('learning_rate', initializer=1e-3, trainable=False)
         opt = tf.train.AdamOptimizer(lr)
-        gradprocs = [MapGradient(lambda grad: tf.clip_by_average_norm(grad, 0.1))]
+        gradprocs = [MapGradient(lambda grad: (tf.clip_by_average_norm(grad, 0.1) if grad is not None else grad))]
         # SummaryGradient()]
         opt = optimizer.apply_grad_processors(opt, gradprocs)
         return opt
@@ -100,9 +129,8 @@ if __name__ == '__main__':
     # else:
     #     dataflow = PrefetchDataZMQ(df, nr_proc=multiprocessing.cpu_count() // 2)
 
-    config = TrainConfig(
-        model=WGAN(),
-        dataflow=df,
+    trainer = WGANTrainer(QueueInput(df), WGAN())
+    trainer.train_with_defaults(
         callbacks=[
             #         ModelSaver(),
             EstimatedTimeLeft(),
@@ -110,9 +138,5 @@ if __name__ == '__main__':
             # ScheduledHyperParamSetter('entropy_beta', [(80, 0.005)]),
             # HumanHyperParamSetter('learning_rate'),
         ],
-        #     session_init=get_model_loader('train_log/auto_encoder/checkpoint'),
         steps_per_epoch=100,
-        max_epoch=100,
-    )
-    trainer = SimpleTrainer()
-    launch_train_with_config(config, trainer)
+        max_epoch=100)
